83b342db71e271b048ee85101218053ea2a56d97
Fransuelton <elton6103@gmail.com>
2024-11-08 07:31:02 -0300

    feat: update index.php to include navigation links

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo "Hello World!";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SGAA - Sistema de Gerência de Associados e Anuidades</title>
+</head>
+
+<body>
+    <h1>SGAA - Sistema de Gerência de Associados e Anuidades</h1>
+    <a href="./views/cadastro_associado.php">Cadastrar Associado</a>
+    <a href="./views/cadastro_anuidade.php">Cadastrar Anuidade</a>
+</body>
+
+</html>
