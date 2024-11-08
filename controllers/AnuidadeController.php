b2d81b87bc690771f7f536badfc4df954757ebb2
Fransuelton <elton6103@gmail.com>
2024-11-08 09:47:57 -0300

    feat: add AnuidadeController

---
+++ controllers/AnuidadeController.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+require_once '../config.php';
+require_once '../models/Anuidade.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $ano = $_POST['ano'];
+    $valor = $_POST['valor'];
+
+    if (!empty($ano) && !empty($valor)) {
+        $anuidade = new Anuidade($conn);
+        $anuidade->cadastrar($ano, $valor);
+
+        $_SESSION['mensagem'] = "Anuidade cadastrada com sucesso!";
+    } else {
+        $_SESSION['mensagem'] = "Ano e valor são obrigatórios!";
+    }
+}
