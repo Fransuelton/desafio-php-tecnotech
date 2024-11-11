8ec1bfb2121fe0e606d15cdce895f63422758313
Fransuelton <elton6103@gmail.com>
2024-11-11 17:34:51 -0300

    feat: redirect to cadastro_anuidade view after processing form submission

--- controllers/AnuidadeController.php
+++ controllers/AnuidadeController.php
@@ -16,4 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     } else {
         $_SESSION['mensagem'] = "Ano e valor são obrigatórios!";
     }
+
+    header("Location: ../views/cadastro_anuidade.php");
+    exit;
 }
