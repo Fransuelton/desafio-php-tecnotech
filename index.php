2b1e3ae1f87e2236ab45d59c6157f95fa468b2f8
Fransuelton <elton6103@gmail.com>
2024-11-11 17:33:43 -0300

    feat: enhance index page layout and add navigation links

--- index.php
+++ index.php
@@ -1,16 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SGAA - Sistema de Gerência de Associados e Anuidades</title>
+    <link rel="stylesheet" href="./assets/css/reset.css">
+    <link rel="stylesheet" href="./assets/css/views/index.css">
+    <link rel="stylesheet" href="./assets/css/variables.css">
 </head>
 
 <body>
-    <h1>SGAA - Sistema de Gerência de Associados e Anuidades</h1>
-    <a href="./views/cadastro_associado.php">Cadastrar Associado</a>
-    <a href="./views/cadastro_anuidade.php">Cadastrar Anuidade</a>
+    <main class="main">
+        <img src="./assets/images/undraw_subscriptions_re_k7jj.svg" alt="unDraw subscription illustration">
+
+        <section class="content-container">
+            <h1 class="title">SGAA - Sistema de Gerência de Associados e Anuidades</h1>
+            <div class="link-container">
+                <a href="./views/cadastro_associado.php">Cadastrar Associado</a>
+                <a href="./views/cadastro_anuidade.php">Cadastrar Anuidade</a>
+                <a href="./views/cobranca.php">Registrar Cobrança</a>
+                <a href="./views/pagamento.php">Pagamento</a>
+                <a href="./views/atrasos.php">Associados em Atraso</a>
+            </div>
+        </section>
+    </main>
+
+    <footer class="footer">
+        <p>Desenvolvido por <a href="">Fransuelton Francisco</a></p>
+    </footer>
 </body>
 
 </html>
